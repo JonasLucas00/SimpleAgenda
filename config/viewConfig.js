713e48f8e6c7c59cfc4d2d864cf4b161f40d80d5
Lucas Jonas <lucas.jonas720@gmail.com>
2025-10-07 19:52:14 -0300

    change in viewConfig.js

--- config/viewConfig.js
+++ config/viewConfig.js
@@ -2,6 +2,6 @@ const path = require('path');
 
 module.exports = function viewConfig(app) {
     app.set('view engine', 'ejs');
-    app.set('views', path.join(process.cwd(), 'src/views')); // process.cwd() = raiz do projeto
-    // console.log(path.join(process.cwd(), 'src/views'));
+    app.set('views', path.join(process.cwd(), 'src', 'views')); // process.cwd() = raiz do projeto
+
 }
