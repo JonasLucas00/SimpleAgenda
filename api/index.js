7dfa64a57dbbccd453d735141f073f6d839ae51a
Lucas Jonas <lucas.jonas720@gmail.com>
2025-10-07 20:09:53 -0300

    Refactor

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(process.cwd(), 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 sessionConfig(app);
 flashConfig(app);
--- config/viewConfig.js
+++ config/viewConfig.js
@@ -1,7 +1,7 @@
 const path = require('path');
+const ejs = require('ejs')
 
 module.exports = function viewConfig(app) {
     app.set('view engine', 'ejs');
-    app.set('views', path.join(process.cwd(), 'src', 'views')); // process.cwd() = raiz do projeto
-
+    app.set('views', path.join(__dirname, '..', 'src', 'views'));
 }
