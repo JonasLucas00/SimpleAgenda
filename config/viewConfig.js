085392940e0ef19e3d26df3a7cdc18a40adad04a
Lucas Jonas <lucas.jonas720@gmail.com>
2025-10-07 16:26:19 -0300

    alteracao viewConfig.js e index.js

--- config/viewConfig.js
+++ config/viewConfig.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
-module.exports = function viewConfig(app){
+module.exports = function viewConfig(app) {
     app.set('view engine', 'ejs');
-    app.set('views', path.join(__dirname, '../src/views'));
-    // console.log(path.join(__dirname, '../src/views'));
+    app.set('views', path.join(process.cwd(), 'src/views')); // process.cwd() = raiz do projeto
+    // console.log(path.join(process.cwd(), 'src/views'));
 }
